e520e437286308be321f91424ca11a41fe46afd1
Airushmeos <benellirider@hotmail.com>
2025-03-10 17:10:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -38,6 +38,68 @@ function createWindow() {
                         shell.openExternal("https://myfirstwebsite.lima-city.at");
                     }
                 },
+                {
+                    label: "E-Mail",
+                    click() {
+                        createWindowemail();
+                    }
+                }
+                // {
+                //     label: "Über",
+                //     click() {
+                //         createAboutWindow();
+                //     }
+                // }
+            ]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
+}
+
+function createWindowemail() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Zweites Fenster (optional, falls benötigt)
+    // const win2 = new BrowserWindow({
+    //     width: 800,
+    //     height: 600,
+    //     webPreferences: {
+    //         nodeIntegration: true
+    //     }
+    // });
+
+    win.loadFile("index2.html"); // Lädt index2.html
+    // win2.loadFile("index.html"); // Lädt index.html, falls aktiv
+
+    // Menüleiste hinzufügen
+    const menu = Menu.buildFromTemplate([
+        {
+            label: "Datei",
+            submenu: [
+                { role: "quit", label: "Beenden" }
+            ]
+        },
+        {
+            label: "Hilfe",
+            submenu: [
+                {
+                    label: "Website besuchen",
+                    click() {
+                        shell.openExternal("https://myfirstwebsite.lima-city.at/senden");
+                    }
+                },
+                {
+                    label: "Roblox",
+                    click() {
+                        createWindow();
+                    }
+                }
                 // {
                 //     label: "Über",
                 //     click() {
